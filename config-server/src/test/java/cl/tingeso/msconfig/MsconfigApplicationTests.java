6a0c3934fc7a3a19a20cf9541fbe787d4b79d3ca
Javier Ignacio Salas Mardones <javiersm@MacBook-Pro.local>
2025-05-04 22:59:35 -0400

    Load config files

---
+++ config-server/src/test/java/cl/tingeso/msconfig/MsconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tingeso.msconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
