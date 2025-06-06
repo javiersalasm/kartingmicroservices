b043a0ab9c38c706233f1ef7fafc9b59650cab23
Javier Ignacio Salas Mardones <javiersm@MacBook-Pro.local>
2025-06-06 13:09:44 -0400

    update gateway-server

--- gateway-server/src/main/java/cl/tingeso/apigateway/config/CorsConfig.java
+++ gateway-server/src/main/java/cl/tingeso/apigateway/config/CorsConfig.java
@@ -1,22 +1,19 @@
 package cl.tingeso.apigateway.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Permite todos los endpoints
-                        .allowedOrigins("http://localhost:5173") // Permite solicitudes solo desde tu React
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                        .allowedHeaders("*"); // Permite todos los headers
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
     }
 }
